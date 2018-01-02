0eff562d6c73995feefd262f8e314a881400636c
Cas <cas@interglobal.vision>
2018-01-02 11:50:15 -0600

    remove unused Logout component

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -4,7 +4,6 @@ import { Switch, Route } from 'react-router-dom';
 import Nav from './Nav';
 import Welcome from './Welcome';
 import Posts from './Posts';
-import Logout from '../components/Logout.jsx';
 import NoMatch from '../components/NoMatch.jsx';
 
 const ControlPanel = () => {
@@ -14,7 +13,6 @@ const ControlPanel = () => {
       <div>
         <Switch>
           <Route path='/posts' component={Posts} />
-          <Route path='/logout' component={Logout} />
           <Route exact path='/' component={Welcome} />
           <Route component={NoMatch}/>
         </Switch>
