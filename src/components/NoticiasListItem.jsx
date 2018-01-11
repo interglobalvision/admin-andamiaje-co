3a89ffcd25324544282a61bae281d565dae512ff
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2018-01-10 21:13:09 -0600

    change list row class to list-rows-item. add publishDateDisplay

--- src/components/NoticiasListItem.jsx
+++ src/components/NoticiasListItem.jsx
@@ -3,23 +3,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { withFirebase } from 'react-redux-firebase';
 
+import moment from 'moment';
+
 const NoticiasListItem = ({ noticia, firebase: { remove } }) => {
   const { key } = noticia;
-  const { title, published } = noticia.value;
+  const { title, published, publishDate } = noticia.value;
+  const publishDateDisplay = moment(publishDate).format('DD-MM-YYYY');
 
   return(
-    <div className='grid-row padding-top-micro padding-bottom-micro align-items-center'>
-      <div className='grid-item item-s-4'>
-        <span><Link to={'/noticias/' + key}>{title}</Link></span>
+    <div className='list-rows-item grid-row padding-top-micro padding-bottom-micro align-items-center'>
+      <div className='grid-item item-s-4 item-l-5'>
+        <span><Link className="link-underline" to={'/noticias/' + key}>{title}</Link></span>
       </div>
-      <div className='grid-item item-s-1'>
+      <div className='grid-item item-s-2'>
         <span>{published ? 'Publicado' : 'Borrador'}</span>
       </div>
-      <div className='grid-item item-s-1 offset-s-5'>
-        <Link to={'/noticias/' + key}>Editar</Link>
+      <div className='grid-item item-s-3 item-l-2'>
+        <span>{publishDateDisplay}</span>
       </div>
       <div className='grid-item item-s-1'>
-        <button onClick={() => window.confirm('¿Seguro que deseas eliminar esta noticia?') ? remove('noticias/' + key) : null}>Eliminar</button>
+        <Link className='font-bold' to={'/noticias/' + key}>Editar</Link>
+      </div>
+      <div className='grid-item item-s-1'>
+        <button className='u-pointer font-bold' onClick={() => window.confirm('¿Seguro que deseas eliminar esta noticia?') ? remove('noticias/' + key) : null}>Eliminar</button>
       </div>
     </div>
   );
