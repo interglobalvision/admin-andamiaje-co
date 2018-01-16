c17d922da0e7b6074741cc40e646eea3c3a336fc
varffs <patrickbest@Patricks-MacBook-Pro.local>
2018-01-16 20:09:16 +0000

    scaffold obras

---
+++ src/components/obras/ObraForm.jsx
@@ -0,0 +1,175 @@
+import React, { Component } from 'react';
+import { firebaseConnect } from 'react-redux-firebase';
+import { withRouter } from 'react-router-dom';
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+@firebaseConnect()
+@withRouter
+class ObraForm extends Component {
+
+  state = {
+    title: '',
+    year: '',
+    artista: '',
+    materials: '',
+    dimensions: '',
+    medium: '',
+    error: {
+      message: '',
+    },
+    isLoading: false,
+  }
+
+  constructor(props) {
+    super(props);
+
+    // If component recieves obra as prop we merge it with initial state (used for editing)
+    this.state = { ...this.state, ...props.obra };
+
+  }
+
+  addObra() {
+    const { title, year, artista, materials, dimensions, medium } = this.state;
+
+    this.setState({ isLoading: true })
+
+    this.props.firebase
+      .push('obras', {
+        title,
+        year,
+        artista,
+        materials,
+        dimensions,
+        medium,
+      })
+      .then(() => {
+        this.setState({ isLoading: false })
+        this.props.history.push('/obras');
+      })
+
+  }
+
+  updateObra() {
+    const { title, year, artista, materials, dimensions, medium } = this.state;
+
+    this.setState({ isLoading: true })
+
+    this.props.firebase
+      .update(`obras/${this.props.id}`, {
+        title,
+        year,
+        artista,
+        materials,
+        dimensions,
+        medium,
+      })
+      .then(() => {
+        this.setState({ isLoading: false })
+      })
+
+  }
+
+  render() {
+    return (
+      <form onSubmit={event => event.preventDefault()}>
+        <div className='grid-row margin-bottom-basic justify-end'>
+          <div className='grid-item'>
+            <button className='button' onClick={() => this.props.id ? this.updateObra() : this.addObra()}>
+              Guardar{ this.props.id ? '' : ' Nuevo'}
+            </button>
+          </div>
+        </div>
+
+        <div className='grid-row margin-bottom-basic'>
+          <div className='grid-item item-s-12'>
+            <h4 className='font-size-small font-bold margin-bottom-tiny'><label htmlFor='title'>Title</label></h4>
+            <input
+              id='title'
+              name='title'
+              type='text'
+              disabled={this.state.isLoading}
+              value={this.state.title}
+              onChange={ event => this.setState({ title: event.target.value })}
+            />
+          </div>
+        </div>
+
+        <div className='grid-row margin-bottom-basic'>
+          <div className='grid-item item-s-12'>
+            <h4 className='font-size-small font-bold margin-bottom-tiny'><label htmlFor='year'>Year</label></h4>
+            <div className='grid-row align-items-center'>
+              <input
+                id='year'
+                name='year'
+                type='text'
+                disabled={this.state.isLoading}
+                value={this.state.year}
+                onChange={ event => this.setState({ year: event.target.value })}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className='grid-row margin-bottom-basic'>
+          <div className='grid-item item-s-12'>
+            <h4 className='font-size-small font-bold margin-bottom-tiny'><label htmlFor='artista'>Artista</label></h4>
+            <input
+              id='artista'
+              name='artista'
+              type='text'
+              disabled={this.state.isLoading}
+              value={this.state.artista}
+              onChange={ event => this.setState({ artista: event.target.value })}
+            />
+          </div>
+        </div>
+
+        <div className='grid-row margin-bottom-basic'>
+          <div className='grid-item item-s-12'>
+            <h4 className='font-size-small font-bold margin-bottom-tiny'><label htmlFor='materials'>Materials</label></h4>
+            <input
+              id='materials'
+              name='materials'
+              type='text'
+              disabled={this.state.isLoading}
+              value={this.state.materials}
+              onChange={ event => this.setState({ materials: event.target.value })}
+            />
+          </div>
+        </div>
+
+        <div className='grid-row margin-bottom-basic'>
+          <div className='grid-item item-s-12'>
+            <h4 className='font-size-small font-bold margin-bottom-tiny'><label htmlFor='dimensions'>Dimensions</label></h4>
+            <input
+              id='dimensions'
+              name='dimensions'
+              type='text'
+              disabled={this.state.isLoading}
+              value={this.state.dimensions}
+              onChange={ event => this.setState({ dimensions: event.target.value })}
+            />
+          </div>
+        </div>
+
+        <div className='grid-row margin-bottom-basic'>
+          <div className='grid-item item-s-12'>
+            <h4 className='font-size-small font-bold margin-bottom-tiny'><label htmlFor='medium'>Medium</label></h4>
+            <input
+              id='medium'
+              name='medium'
+              type='text'
+              disabled={this.state.isLoading}
+              value={this.state.medium}
+              onChange={ event => this.setState({ medium: event.target.value })}
+            />
+          </div>
+        </div>
+
+      </form>
+    );
+  }
+};
+
+export default ObraForm;
