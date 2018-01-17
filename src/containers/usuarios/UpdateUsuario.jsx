adef0ba8ec470ac367dd45f7c318d2189d80e6a9
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2018-01-16 18:29:17 -0600

    update page title

--- src/containers/usuarios/UpdateUsuario.jsx
+++ src/containers/usuarios/UpdateUsuario.jsx
@@ -16,7 +16,7 @@ const UpdateUsuario = ({ firebase, usuario, id }) => {
       <section>
         <header className='grid-row margin-bottom-basic'>
           <div className='grid-item'>
-            <h1 className='font-size-large'>Editar Entrada</h1>
+            <h1 className='font-size-large'>Editar Usuario</h1>
           </div>
         </header>
 
