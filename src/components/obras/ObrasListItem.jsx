21b5b0bf2ea7e6b65f30f0fadb245b4a0f1bdc78
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2018-01-25 17:24:40 -0600

    add removeObra function with toastr confirm

--- src/components/obras/ObrasListItem.jsx
+++ src/components/obras/ObrasListItem.jsx
@@ -2,10 +2,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withFirebase } from 'react-redux-firebase';
+import { toastr } from 'react-redux-toastr';
+
+import { ToastrConfirmOptions, ToastrOptionsSuccess } from '../../utilities/constants.js';
 
 const ObrasListItem = ({ obra, firebase: { remove } }) => {
   const { key } = obra;
   const { title, year, medium } = obra.value;
+
+  const removeObra = (key) => {
+
+    remove('obras/' + key)
+    .then(() => {
+
+      // Display success toast
+      toastr.success('Éxito', 'Obra eliminado', ToastrOptionsSuccess);
+
+    });
+  };
 
   return(
     <div className='list-rows-item grid-row padding-top-micro padding-bottom-micro align-items-center'>
@@ -23,7 +37,7 @@ const ObrasListItem = ({ obra, firebase: { remove } }) => {
           <Link className='font-bold' to={'/obras/' + key}>Editar</Link>
         </div>
         <div className='grid-item'>
-          <button className='u-pointer font-bold' onClick={() => window.confirm('¿Seguro que deseas eliminar esta noticia?') ? remove('artistas/' + key) : null}>Eliminar</button>
+          <button className='u-pointer font-bold' onClick={() => toastr.confirm('¿Seguro que deseas eliminar esta obra?', ToastrConfirmOptions(removeObra, key))}>Eliminar</button>
         </div>
       </div>
     </div>
