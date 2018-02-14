67e0daab4d5b68adf4199b125c3763dca33d08b0
Cas <cas@interglobal.vision>
2018-02-13 20:25:16 -0600

    add .env.production, add api keuys and urls to .env's, set firebase to have to enviroments

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { reactReduxFirebase } from 'react-redux-firebase'
 
 // Firebae configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDfnoTqolYAjy2WvuxsPGJ7t7oycUjAVR4',
-  authDomain: 'igv-andamiaje-co.firebaseapp.com',
-  databaseURL: 'https://igv-andamiaje-co.firebaseio.com',
-  projectId: 'igv-andamiaje-co',
-  storageBucket: 'igv-andamiaje-co.appspot.com',
-  messagingSenderId: '796880348739',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABSE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Init firebase instance
