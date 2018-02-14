d78768b51b909b1793feea6d1bddd1d1fd47c629
varffs <patrickbest@Patricks-MacBook-Pro.local>
2018-02-14 23:34:54 +0000

    hide up/down buttons when neither can be used

--- src/components/fields/ImageUploadsImage.jsx
+++ src/components/fields/ImageUploadsImage.jsx
@@ -16,18 +16,21 @@ class ImageUploadsImage extends Component {
     }
   }
 
+
   render() {
     return(
       <div key={this.props.image.key} className='list-rows-item grid-row padding-top-micro padding-bottom-micro align-items-center'>
         <div className='grid-item item-s-6'>
           <img src={this.imageUrl} alt='' />
         </div>
+
         <div className='grid-item flex-grow grid-row justify-end'>
-          <button type='button' onClick={() => this.props.moveUp(this.props.image)} disabled={this.props.upDisabled} className='button button-small'>↑</button>
-          <button type='button' onClick={() => this.props.moveDown(this.props.image)} disabled={this.props.downDisabled} className='button button-small'>↓</button>
+          { this.props.upDisabled && this.props.downDisabled ? '' : <button type='button' onClick={() => this.props.moveUp(this.props.image)} disabled={this.props.upDisabled} className='button button-small'>↑</button> }
+          { this.props.upDisabled && this.props.downDisabled ? '' : <button type='button' onClick={() => this.props.moveDown(this.props.image)} disabled={this.props.downDisabled} className='button button-small'>↓</button> }
 
           <button type='button' className='button button-small button-delete' onClick={() => toastr.confirm('¿Seguro que deseas eliminar esta imagen?', ToastrOptionsConfirm(this.props.delete, this.props.image))}>Eliminar</button>
         </div>
+
       </div>
     );
   }
