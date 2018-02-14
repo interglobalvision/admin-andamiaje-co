3bc67dca60e7bca7f7ac7c0540f58c6877a63b05
mikeyrayvon <mikey@interglobal.vision>
2018-02-13 19:19:47 -0600

    add loadingStatusReducer to appReducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -6,11 +6,13 @@ import { firebaseStateReducer } from 'react-redux-firebase'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import { uploadStatusReducer } from './reducers/uploadStatusReducer';
+import { loadingStatusReducer } from './reducers/loadingStatusReducer';
 
 const appReducer = combineReducers({
   firebase: firebaseStateReducer,
   uploadStatus: uploadStatusReducer,
   toastr: toastrReducer,
+  loadingStatus: loadingStatusReducer,
 })
 
 // Setup root reducer
