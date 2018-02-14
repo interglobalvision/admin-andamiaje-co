26754ef172380f0024259a9e1e65a47313c5aaab
mikeyrayvon <mikey@interglobal.vision>
2018-02-13 20:18:05 -0600

    add loading status component

---
+++ src/components/LoadingStatus.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withFirebase } from 'react-redux-firebase';
+
+const LoadingStatus = ({ loadingStatus }) => {
+  return (
+    <div id='loading-status' className={loadingStatus ? 'grid-row align-items-center justify-center loading' : 'grid-row align-items-center justify-center'}>
+      <div className='font-size-large'>Loading...</div>
+    </div>
+  );
+};
+
+export default LoadingStatus;
