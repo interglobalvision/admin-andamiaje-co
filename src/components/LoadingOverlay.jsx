692dcacfa58f3458aae5fe7d69b15dab3cc25ee5
mikeyrayvon <mikey@interglobal.vision>
2018-02-13 20:22:49 -0600

    add loading overlay styles

--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -3,7 +3,7 @@ import { withFirebase } from 'react-redux-firebase';
 
 const LoadingOverlay = ({ loadingStatus }) => {
   return (
-    <div id='loading-status' className={loadingStatus ? 'grid-row align-items-center justify-center loading' : 'grid-row align-items-center justify-center'}>
+    <div id='loading-overlay' className={loadingStatus ? 'grid-row align-items-center justify-center loading' : 'grid-row align-items-center justify-center'}>
       <div className='font-size-large'>Loading...</div>
     </div>
   );
