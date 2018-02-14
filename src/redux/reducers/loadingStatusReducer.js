234ddb289b9d3d69a4c7588159e8419a8a7b5139
mikeyrayvon <mikey@interglobal.vision>
2018-02-14 11:11:07 -0600

    remove unused constants import

--- src/redux/reducers/loadingStatusReducer.js
+++ src/redux/reducers/loadingStatusReducer.js
@@ -1,11 +1,3 @@
-// This is a custom reducer used to change the state based on the loading
-// actions defined by react-redux-firebase, since natively they don't have a
-// reducer yet.
-// For updates check: https://github.com/prescottprue/react-redux-firebase/issues/346
-
-// Import constants directly from react-redux-firebase
-import { constants } from 'react-redux-firebase';
-
 // This is our initial state
 const initialState = {
   loading: false
