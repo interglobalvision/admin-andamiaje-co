b8d089c94b474f928cc8890896f7a9d060ad7fd2
mikeyrayvon <mikey@interglobal.vision>
2018-02-14 19:22:46 -0600

    close #56. disable group Add button if selected empty. set selected to empty object on select change if value empty.

--- src/components/obras/ObrasGroup.jsx
+++ src/components/obras/ObrasGroup.jsx
@@ -35,6 +35,14 @@ class ObrasGroup extends Component {
   }
 
   handleSelectChange(event) {
+    if (event.target.options[event.target.selectedIndex].value === '') {
+      this.setState({
+        selectedObra: {},
+      });
+
+      return;
+    }
+
     const id = event.target.options[event.target.selectedIndex].value;
 
     const { value: { artista, title, year, materials, tecnica } } = this.props.allObras.find( obra => obra.key === id );
@@ -87,7 +95,7 @@ class ObrasGroup extends Component {
                 </div>
               </div>
               <div className='grid-item'>
-                <button className='button' type='button' onClick={this.addObraToGroup}>Agregar</button>
+                <button className='button' type='button' disabled={!Object.keys(this.state.selectedObra).length ? 'disabled' : ''} onClick={this.addObraToGroup}>Agregar</button>
               </div>
             </div>
           }
