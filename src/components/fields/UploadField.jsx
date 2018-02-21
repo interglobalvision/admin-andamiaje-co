57a6947f1b32dca1234b3ad27d282a4d9cba964e
varffs <patrickbest@Patricks-MacBook-Pro.local>
2018-02-21 20:55:51 +0000

    sanitize filename with package

--- src/components/fields/UploadField.jsx
+++ src/components/fields/UploadField.jsx
@@ -5,6 +5,8 @@ import { toastr } from 'react-redux-toastr';
 
 import Dropzone from 'react-dropzone'; // For Dropzone reference check https://github.com/react-dropzone/react-dropzone
 
+import sanitize from 'sanitize-filename';
+
 import { ToastrOptionsError } from '../../utilities/toastr';
 import { loadImageSizes } from '../../utilities/images';
 
@@ -18,7 +20,9 @@ class UploadField extends Component {
   uploadOptions = {
     progress: false, // disabled until the renaming bug is fixed in react-redux-firebase
     name: (file) => {
-      return Date.now() + '-' + file.name;
+      let filename = sanitize(file.name);
+
+      return Date.now() + '-' + filename;
     }
   }
 
