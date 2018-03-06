60a12aeaabe79563255f46be5909cedda2ffcfa9
mikeyrayvon <mikey@interglobal.vision>
2018-03-06 10:51:09 -0600

    add user wishlist to user form

---
+++ src/components/usuarios/UsuarioWishlist.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import UsuarioWishlistItem from '../../components/usuarios/UsuarioWishlistItem';
+
+const UsuarioWishlist = ({ wishlist }) => {
+  return (
+    <section className="margin-bottom-basic">
+      <header className='grid-row margin-bottom-tiny font-size-small font-bold'>
+        <h4 className='grid-item item-s-12 font-size-small font-bold margin-bottom-tiny'>Wishlist</h4>
+        <div className='grid-item item-s-6'>
+          <h5>Artista</h5>
+        </div>
+        <div className='grid-item item-s-6'>
+          <h5>Titulo</h5>
+        </div>
+      </header>
+
+      <div className="list-rows-holder">
+        { Object.keys(wishlist).map(
+          (key, id) => <UsuarioWishlistItem key={key} id={id} item={wishlist[key]} />
+        ) }
+      </div>
+    </section>
+  );
+};
+
+export default UsuarioWishlist;
